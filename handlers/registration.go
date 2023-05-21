d73dd5fae57d65e8df106e83746a66b368c91a19
Olena <olenamaximiv@gmail.com>
2023-05-20 18:04:36 -0600

    Added correct registration form

--- handlers/registration.go
+++ handlers/registration.go
@@ -2,19 +2,67 @@ package handlers
 
 import (
 	"database/sql"
+
+	"html/template"
+	"log"
 	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
+// RegisterHandler handles the registration functionality
 func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		// Display the registration form
-		http.ServeFile(w, r, "templates/registration.html")
+		tmpl, err := template.ParseFiles("templates/registration.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	} else if r.Method == http.MethodPost {
 		// Parse form values
 		username := r.FormValue("username")
+		email := r.FormValue("email")
 		password := r.FormValue("password")
+		confirmPassword := r.FormValue("confirm_password")
+
+		// Validate form inputs
+		formErrors := make(map[string]string)
+		if username == "" {
+			formErrors["username"] = "The username is required and cannot be empty"
+		}
+		if email == "" {
+			formErrors["email"] = "The email is required and cannot be empty"
+		}
+		if password == "" {
+			formErrors["password"] = "The password is required and cannot be empty"
+		}
+		if confirmPassword == "" {
+			formErrors["confirm_password"] = "The confirm password is required and cannot be empty"
+		}
+		if password != confirmPassword {
+			formErrors["confirm_password"] = "The password and its confirm are not the same"
+		}
+
+		if len(formErrors) > 0 {
+			// Display the registration form with form errors
+			tmpl, err := template.ParseFiles("templates/registration.html")
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			err = tmpl.Execute(w, formErrors)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			return
+		}
 
 		// Establish the database connection
 		db, err := sql.Open("postgres", "user=postgres password=postgres dbname=blog sslmode=disable")
@@ -25,20 +73,23 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 		defer db.Close()
 
 		// Insert new user into the database
-		stmt, err := db.Prepare("INSERT INTO users (username, password) VALUES ($1, $2)")
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		defer stmt.Close()
-
-		_, err = stmt.Exec(username, password)
+		_, err = db.Exec("INSERT INTO users (username, email, password) VALUES ($1, $2, $3)", username, email, password)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
-		// Redirect to a success page or another endpoint
-		http.Redirect(w, r, "/success", http.StatusSeeOther)
+		// Redirect to the home page with a success message
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
+}
+
+func main() {
+	// Start the HTTP server
+	log.Println("Server started on :8080")
+	http.HandleFunc("/registration", RegisterHandler)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
 	}
 }
--- main.go
+++ main.go
@@ -5,51 +5,48 @@ import (
 	"html/template"
 	"github.com/OlenaMaximiv/blog/handlers"
 	_ "github.com/lib/pq"
-	"log"
-	"fmt"
-	"database/sql"
 )
 
-func InitializeDB() error {
-	// Connection parameters
-	host := "localhost"
-	port := 5432
-	user := "postgres"
-	password := "postgres"
-	dbname := "blog"
+// func InitializeDB() error {
+// Connection parameters
+//	host := "localhost"
+//	port := 5432
+//	user := "postgres"
+//	password := "postgres"
+//	dbname := "blog"
 
-	// Connection string
-	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+// Connection string
+//	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	// Open a database connection
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		return err
-	}
+// Open a database connection
+//	db, err := sql.Open("postgres", connStr)
+//	if err != nil {
+//		return err
+//	}
 
-	// Ping the database to check the connection
-	err = db.Ping()
-	if err != nil {
-		return err
-	}
+// Ping the database to check the connection
+//	err = db.Ping()
+//	if err != nil {
+//		return err
+//	}
 
-	// Print a success message
-	log.Println("Connected to the database!")
+// Print a success message
+//	log.Println("Connected to the database!")
 
-	// Close the database connection
-	defer db.Close()
+// Close the database connection
+//	defer db.Close()
 
-	// Additional database initialization logic
-	// ...
+// Additional database initialization logic
+// ...
 
-	return nil
-}
+//	return nil
+// }
 
 func main() {
-	err := InitializeDB()
-	if err != nil {
-		log.Fatal(err)
-	}
+	//	err := InitializeDB()
+	//	if err != nil {
+	//		log.Fatal(err)
+	//	}
 
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/comment", handlers.CommentHandler)
@@ -58,14 +55,11 @@ func main() {
 	http.HandleFunc("/logout", handlers.LogoutHandler)
 	http.HandleFunc("/post", handlers.PostHandler)
 	http.HandleFunc("/profile", handlers.ProfileHandler)
-	http.HandleFunc("/registration", handlers.RegisterHandler)
-
-
+	http.HandleFunc("/register", handlers.RegisterHandler)
 	http.HandleFunc("/reset-password", handlers.ResetPasswordHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
-
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// Parse the home page template
@@ -87,49 +81,48 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	// ...
 }
 
-
 func loginHandler(w http.ResponseWriter, r *http.Request) {
-    if r.Method == "GET" {
-        // Parse the login page template
-        tmpl := template.Must(template.ParseFiles("templates/login.html"))
+	if r.Method == "GET" {
+		// Parse the login page template
+		tmpl := template.Must(template.ParseFiles("templates/login.html"))
 
-        // Execute the template
-        err := tmpl.Execute(w, nil)
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-    } else if r.Method == "POST" {
-        // Handle the login form submission
-        // Retrieve the username and password from the request form values
-        username := r.FormValue("username")
-        password := r.FormValue("password")
+		// Execute the template
+		err := tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	} else if r.Method == "POST" {
+		// Handle the login form submission
+		// Retrieve the username and password from the request form values
+		username := r.FormValue("username")
+		password := r.FormValue("password")
 
-        // Perform the login logic here
-        if username == "admin" && password == "password" {
-            // Successful login
-            // Redirect the user to another page after successful login
-            http.Redirect(w, r, "/profile", http.StatusFound)
-        } else {
-            // Failed login
-            // Render the login page with an error message
-            tmpl := template.Must(template.ParseFiles("templates/login.html"))
+		// Perform the login logic here
+		if username == "admin" && password == "password" {
+			// Successful login
+			// Redirect the user to another page after successful login
+			http.Redirect(w, r, "/profile", http.StatusFound)
+		} else {
+			// Failed login
+			// Render the login page with an error message
+			tmpl := template.Must(template.ParseFiles("templates/login.html"))
 
-            // Create a data structure to pass error message to the template
-            data := struct {
-                Username string
-                ErrorMessage string
-            }{
-                Username: username,
-                ErrorMessage: "Invalid username or password",
-            }
+			// Create a data structure to pass error message to the template
+			data := struct {
+				Username     string
+				ErrorMessage string
+			}{
+				Username:     username,
+				ErrorMessage: "Invalid username or password",
+			}
 
-            // Execute the template with the data
-            err := tmpl.Execute(w, data)
-            if err != nil {
-                http.Error(w, err.Error(), http.StatusInternalServerError)
-                return
-            }
-        }
-    }
+			// Execute the template with the data
+			err := tmpl.Execute(w, data)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+		}
+	}
 }
